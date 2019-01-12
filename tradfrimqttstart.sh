e85bf061ba2e76ba71155eb182a221334edd97ca
softypit <paul_tupper@hotmail.com>
2019-01-12 22:54:35 +0000

    add demo startup script

--- tradfrimqttstart.sh
+++ tradfrimqttstart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 start() {
-    python /usr/bin/tradfri/tradfri_mqtt.py 192.168.1.110 Paul tspKRgpwwWIwozbS 192.168.1.229 1883 &
+    python /usr/bin/tradfri/tradfri_mqtt.py <tradfri gateway ipaddr here> <username> <key> <mqtt broker url> <mqtt broker port> &
     mypid = $!
     echo -n $mypid > /var/tradfri_mqtt_pid
 }
